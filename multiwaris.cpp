b3de0458d2bc7b507724e9ab57c43da5ab052ca0
jeeyyxs <riodwimulyono0512@gmail.com>
2026-08-17 23:09:08 +0700

    membuat classpelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -14,8 +14,17 @@ public:
 class pekerja : virtual public orang {
 public:
     pekerja(int pUmur) :
-    orang(pUmur)
+        orang(pUmur)
     {
         cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
     }
 };
